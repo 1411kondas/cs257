dc2a5106694adf0e2fbf2481009ead48453cbecd
1411kondas <90916512+1411kondas@users.noreply.github.com>
2021-10-01 14:40:10 -0500

    October 1- Task #3- First Draft of Code for booksdatasource.py

--- Books/booksdatasource.py
+++ Books/booksdatasource.py
@@ -49,7 +49,39 @@ class BooksDataSource:
             suitable instance variables for the BooksDataSource object containing
             a collection of Author objects and a collection of Book objects.
         '''
-        pass
+        global listOfAllAuthors
+        self.listOfAllAuthors = []
+        global ListOfAllBooks
+        self.listOfAllBooks = []
+        file = open(books_csv_file_name)
+        reader = csv.reader(file)
+        for row in reader:
+            title = row[0]
+            pub_year = int(row[1])
+            auth_string = row[2]
+
+            authorAttributes = auth_string.split(' ')
+            if len(authorAttributes) == 3:
+                authorFirstName, authorLastName, authorYearsString = authorAttributes[0], authorAttributes[1], authorAttributes[2]
+            if len(authorAttributes) == 4:
+                authorFirstName, authorLastName, authorYearsString = authorAttributes[0], authorAttributes[1] + ' ' + authorAttributes[2], authorAttributes[3]
+
+# Need to implement something about the book that has 2 authors :)
+
+            authorYearsString.strip('('')')
+            authorYears = authorYearsString.split('-')
+            birthYear = authorYears[0]
+            if len(authorYears) == 2:
+                deathYear = authorYears[1]
+            authorObject = Author(authorFirstName, authorLastName, birthYear, deathYear)
+            # add authorObject to listOfAllAuthors
+            self.listOfAllAuthors.append(authorObject)
+
+            bookObject = Book(title, pub_year, authorObject)
+            # add bookObject to listOfAllBooks
+            self.listOfAllBooks.append(bookObject)
+        file.close
+
 
     def authors(self, search_text=None):
         ''' Returns a list of all the Author objects in this data source whose names contain
@@ -57,7 +89,25 @@ class BooksDataSource:
             returns all of the Author objects. In either case, the returned list is sorted
             by surname, breaking ties using given name (e.g. Ann Brontë comes before Charlotte Brontë).
         '''
-        return []
+        authorList = []
+        searchLower = search_text.lower()
+
+        if searchLower == '':
+            authorList = BooksDataSource.listOfAllAuthors
+        # search text is none, return all Author objects
+
+        else:
+            for author in BooksDataSource.listOfAllAuthors:
+                if searchLower in author.surname.lower() or author.given_name.lower():
+                    authorList.append(author)
+
+        authorList.sort(key=lambda auth: (auth.suname, auth.given_name))
+        # sort result List alphabetically
+            #if last names are equal, sort by given name
+
+        return authorList
+
+
 
     def books(self, search_text=None, sort_by='title'):
         ''' Returns a list of all the Book objects in this data source whose
@@ -71,7 +121,27 @@ class BooksDataSource:
                 default -- same as 'title' (that is, if sort_by is anything other than 'year'
                             or 'title', just do the same thing you would do for 'title')
         '''
-        return []
+        bookList = []
+        searchLower = search_text.lower()
+
+        if searchLower == '':
+            bookList = BooksDataSource.listOfAllBooks
+        # search text is none, return all book objects -- sorted alphabetically
+
+        else:
+            for book in BooksDataSource.listOfAllBooks:
+                if searchLower in book.title.lower():
+                    bookList.append(book)
+
+        if sort_by == 'year':
+            bookList.sort(key=lambda book: (book.publication_year, book.title))
+
+        else: #otherwise it is null or title or random string, so do by title
+            bookList.sort(key=lambda book: (book.title, book.publication_year))
+
+
+        return bookList
+
 
     def books_between_years(self, start_year=None, end_year=None):
         ''' Returns a list of all the Book objects in this data source whose publication
@@ -84,5 +154,22 @@ class BooksDataSource:
             during start_year should be included. If both are None, then all books
             should be included.
         '''
-        return []
+
+        bookList = []
+        if start_year == '': #because we instantiated publication_year as int
+            startYearInt = 0 #out of range of list
+        else:
+            startYearInt = int(start_year)
+
+        if end_year == '': #because we instantiated publication_year as int
+            endYearInt = 2050 #out of range of list
+        else:
+            endYearInt = int(end_year)
+
+        for book in BooksDataSource.listOfAllBooks:
+            if startYearInt <= book.publication_year <= endYearInt:
+                bookList.append(book)
+
+        bookList.sort(key=lambda book: (book.publication_year, book.title))
+        return bookList
 
