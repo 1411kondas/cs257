f2565fe8a0a11d16a555ec698c6ea0c6accb012b
1411kondas <90916512+1411kondas@users.noreply.github.com>
2021-10-14 13:42:39 -0500

    Update convert.py

--- olympics/convert.py
+++ olympics/convert.py
@@ -14,45 +14,45 @@ import noc_region.csv
 
 
 file = open('athlete_events.csv')
-    reader = csv.reader(file)
-        for row in reader:
-            id = row[0]
-            full_name = row[1]
-            sex = row[2]
-            age = int(row[3])
-            height = int(row[4])
-            weight = int(row[5])
-            team = row[6]
-            NOC = row[7]
-            games = row[8]
-            year = int(row[9])
-            season = row[10]
-            city = row[11]
-            sport = row[12]
-            sport = row[13]
-            medal = row[14]
+reader = csv.reader(file)
+for row in reader:
+    id_number = row[0]
+    full_name = row[1]
+    sex = row[2]
+    age = int(row[3])
+    height = int(row[4])
+    weight = int(row[5])
+    team = row[6]
+    NOC = row[7]
+    games = row[8]
+    year = int(row[9])
+    season = row[10]
+    city = row[11]
+    sport = row[12]
+    sport = row[13]
+    medal = row[14]
 
-            #names_seperates = full_name.split(' ')
-            #    firstName = names_seperates[0]
-            #    lastName = names_seperates[0]
+    #names_seperates = full_name.split(' ')
+    #    firstName = names_seperates[0]
+    #    lastName = names_seperates[0]
 
 file = open('noc_region.csv')
-    reader = csv.reader(file)
-        for row in reader:
-            NOC = row[0]
-            region = row[1]
-            notes = row[2]
+reader = csv.reader(file)
+for row in reader:
+    NOC = row[0]
+    region = row[1]
+    notes = row[2]
 
 
 athlete.csv = open('athlete_events.csv', 'w')
     writer = csv.writer(athlete.csv)
-    writer.writerow(id)
+    writer.writerow(id_number)
     writer.writerow(full_name)
 athlete.csv.close()
 
 athlete_biodata.csv = open('athlete_events.csv', 'w')
     writer = csv.writer(athlete_biodata.csv)
-    writer.writerow(id)
+    writer.writerow(id_number)
     writer.writerow(sex)
     writer.writerow(age)
     writer.writerow(height)
@@ -61,7 +61,7 @@ athlete_biodata.csv.close()
 
 athlete_team.csv = open('athlete_events.csv', 'w')
     writer = csv.writer(athlete_team.csv)
-    writer.writerow(id)
+    writer.writerow(id_number)
     writer.writerow(team)
     writer.writerow(NOC)
 athlete_team.csv.close()
@@ -82,7 +82,7 @@ game.csv.close()
 
 athlete_event.csv = open('athlete_events.csv', 'w')
     writer = csv.writer(athlete_event.csv)
-    writer.writerow(id)
+    writer.writerow(id_number)
     writer.writerow(games)
     writer.writerow(sport)
     writer.writerow(event)
