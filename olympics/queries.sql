161045dd505da28d029a77af1ad1036e8eb7c6fa
1411kondas <90916512+1411kondas@users.noreply.github.com>
2021-10-21 18:48:10 -0500

    Update queries.sql

--- olympics/queries.sql
+++ olympics/queries.sql
@@ -1,14 +1,12 @@
 #List all the medals won by Greg Louganis, sorted by year.
 #Include whatever fields in this output that you think appropriate.
 
-SELECT athlete_event.games, athlete_event.sport, athlete_event.event, athlete_event.medal
-FROM athlete_event,  athlete
-WHERE athlete.id = athlete_event.id
-AND athlete_event.medal IS NOT NULL
+SELECT athlete_game.games, athlete_game.sport, athlete_game.event, athlete_game.medal
+FROM athlete, athlete_game
+WHERE athlete.id = athlete_game.id
+AND athlete_game.medal IS NOT NULL
 AND athlete.full_name = 'Greg Louganis'
-ORDER BY athlete_event.year;
-
-
+ORDER BY athlete_game.year;
 
 
 
@@ -17,29 +15,26 @@ ORDER BY athlete_event.year;
 #you might find that a portion of a country
 #participated in a particular games (e.g. one guy from Newfoundland in 1904) or some other oddball situation
 
-SELECT region_NOC.NOC
-FROM region_NOC
-WHERE region_NOC.NOC IS NOT NULL
-ORDER BY region_NOC.NOC ASC;
-
-
+SELECT team_NOC.NOC
+FROM team_NOC
+WHERE team_NOC.NOC IS NOT NULL
+ORDER BY team_NOC.NOC ASC;
 
 
 
 #List the names of all the athletes from Kenya.
 #If your database design allows it, sort the athletes by last name.
 
-SELECT athlete_data.team, athlete.full_name
+SELECT athlete.full_name
 FROM athlete_data, athlete
-WHERE athlete_data.team = 'Kenya';
-
-
+WHERE athlete.id = athlete_data.id 
+AND athlete_data.team = 'Kenya';
 
 
 
 #List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
 
 SELECT medal.NOC, medal.gold
-FROM medal.gold, medal.NOC
+FROM medal
 WHERE medal.gold IS NOT NULL
 ORDER BY medal.gold DESC;
